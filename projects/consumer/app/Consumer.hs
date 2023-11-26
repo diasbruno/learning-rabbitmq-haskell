68b111faf1b63400d46ada006a5068b2b7c506f3
Bruno Dias <dias.h.bruno@gmail.com>
2023-11-25 22:03:22 -0300

    added missing ack after consume the message.

--- projects/consumer/app/Consumer.hs
+++ projects/consumer/app/Consumer.hs
@@ -5,11 +5,12 @@ module Main where
 import Control.Monad (void)
 import qualified Data.ByteString.Lazy.Char8 as BL
 import MyLib (initChannel, localQueueName, withConnection)
-import Network.AMQP (Ack (Ack), Envelope, Message (msgBody), consumeMsgs)
+import Network.AMQP (Ack (Ack), Envelope, Message (msgBody), consumeMsgs, ackEnv)
 
 myCallback :: (Message, Envelope) -> IO ()
-myCallback (msg, _env) = do
+myCallback (msg, env) = do
   putStrLn $ "received: " ++ BL.unpack (msgBody msg)
+  ackEnv env
 
 main :: IO ()
 main =
